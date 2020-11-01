d65f068811a10f6cdf6895ea679f5d019ce69536
latp <tarradas.adria@gmail.com>
2020-11-01 19:54:57 +0100

    Updated with chef's urls

--- main.py
+++ main.py
@@ -16,8 +16,14 @@ for i in range(1,5):
 # Obtenim la url i el nom dels cuiners
 for a in soup.find_all("section", {"class": [class_sections]}):
 	for b in a.find_all(["h2","h1"]):
-		print(b.a['title'])
-		print(b.a['href'])
+		nom_cuiner = b.a['title']
+		pagina_cuiner = b.a['href']
+		if b.a['href'].find('https') == -1:
+			pagina_cuiner = 'https://www.hogarmania.com' + b.a['href']
+		print(nom_cuiner)
+		print(pagina_cuiner)
 		print("\n\n")
 		
 # Per cada cuiner obtenim totes les url de les seves receptes
+# n'hi ha que tenen nom relatiu i n'hi ha que tenen nom complet
+# cal detectar i accedir a la 
