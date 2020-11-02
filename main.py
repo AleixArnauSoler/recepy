51757570052a9d1491e047de76af46712bbd540f
latp <tarradas.adria@gmail.com>
2020-11-02 14:43:33 +0100

    Cuiners retrieved as a map

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 url_cuniers = "https://www.hogarmania.com/cocina/cocineros/"
 page = requests.get(url_cuniers)
 soup = BeautifulSoup(page.content)
+cuiners = {}
 
 # Guardem a la llista class_sections el nom de les seccions que contenen url i nom de cuiners
 class_sections = ["destacado"]
@@ -20,10 +21,26 @@ for a in soup.find_all("section", {"class": [class_sections]}):
 		pagina_cuiner = b.a['href']
 		if b.a['href'].find('https') == -1:
 			pagina_cuiner = 'https://www.hogarmania.com' + b.a['href']
-		print(nom_cuiner)
-		print(pagina_cuiner)
-		print("\n\n")
-		
+		cuiners[nom_cuiner] = pagina_cuiner
+
+# Llista dels cuiners		
+for cuiner in cuiners:
+	# De cada cuiner volem el nom i la pagina per separat
+	# Tenim el nom del cuiner i la pagina on te les receptes
+	# Agafem les receptes del cuiner i les guardem a un map
+	# Recepta:
+	#	- Cuiner
+	#	- Titol
+	# 	- Ingredients
+	# 	- Enllaç video
+	# 	- 
+	#	- 
+	
+	print(value)
+	print(cuiners[value])
+	print("\n\n")
+
+
 # Per cada cuiner obtenim totes les url de les seves receptes
 # n'hi ha que tenen nom relatiu i n'hi ha que tenen nom complet
 # cal detectar i accedir a la 
